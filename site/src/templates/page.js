ced875ae8da98fbc4b9b71c3390090d46495114d
Dustin Schau <dustinschau@gmail.com>
2020-02-01 15:58:22 -0800

    choree: update

--- site/src/templates/page.js
+++ site/src/templates/page.js
@@ -1,6 +1,7 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import { graphql } from 'gatsby'
+import { jsx, Styled } from 'theme-ui'
+import React from 'react'
+import { graphql, Link } from 'gatsby'
 import Image from 'gatsby-image'
 
 import Layout from '../components/layout'
@@ -27,6 +28,58 @@ function Page({ data }) {
         title="Wedding | August 8, 2020"
       />
       {page.hero && <Image fluid={page.hero[0].fluid} />}
+      {data.page.slug === `/` && (
+        <React.Fragment>
+          <Styled.div sx={{ textAlign: `center`, pt: `10vh`, pb: `10vh` }}>
+            <Styled.p
+              sx={{ fontStyle: `italic`, fontSize: 24, fontFamily: `heading` }}
+            >
+              Please join us for our wedding celebration on
+            </Styled.p>
+            <Styled.h1
+              sx={{
+                fontSize: 48,
+                padding: 2,
+                mb: 0,
+                textTransform: `uppercase`,
+              }}
+            >
+              August 8, 2020
+            </Styled.h1>
+            <Styled.h2 sx={{ fontSize: 40, fontFamily: `Parisienne` }}>
+              Minneapolis, MN
+            </Styled.h2>
+            {process.env.GATSBY_SHOW_RSVP !== `false` && (
+              <Link
+                to="/rsvp/"
+                sx={{
+                  ':hover': {
+                    borderColor: `text`,
+                    backgroundColor: `background`,
+                    color: `text`,
+                  },
+                  borderWidth: 4,
+                  borderColor: `transparent`,
+                  borderStyle: `solid`,
+                  display: `inline-block`,
+                  fontSize: 32,
+                  textDecoration: `none`,
+                  backgroundColor: `text`,
+                  color: `background`,
+                  pt: 3,
+                  pb: 3,
+                  pr: 4,
+                  pl: 4,
+                  mt: 2,
+                  mb: 2,
+                }}
+              >
+                RSVP
+              </Link>
+            )}
+          </Styled.div>
+        </React.Fragment>
+      )}
       {page.moments &&
         page.moments.map(moment => (
           <Timeline key={moment.id} moments={moment} />
@@ -46,6 +99,7 @@ export const pageQuery = graphql`
   query PageBySlug($slug: String!) {
     page: contentfulPage(fields: { slug: { eq: $slug } }) {
       id
+      slug
       contentBlocks {
         ... on ContentfulGallery {
           photos {
