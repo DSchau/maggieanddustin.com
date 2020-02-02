9224836f21e509b7c36ee9cde49b3ebd493ae5ec
Dustin Schau <dustinschau@gmail.com>
2020-02-01 17:19:44 -0800

    chore: small twaek

--- site/src/components/partials/index.js
+++ site/src/components/partials/index.js
@@ -1,7 +1,9 @@
 import Home from './home'
+import Proposal from './proposal'
 import Venue from './venue'
 
 export default {
   '/': Home,
+  proposal: Proposal,
   venue: Venue,
 }
---
+++ site/src/components/partials/proposal.js
@@ -0,0 +1,23 @@
+/** @jsx jsx */
+import { jsx, Styled } from 'theme-ui'
+
+export default function Proposal() {
+  return (
+    <Styled.div
+      sx={{ textAlign: `center`, pt: `10vh`, pb: `calc(10vh - 38px)` }}
+    >
+      <Styled.h1
+        sx={{ fontSize: 48, padding: 2, mb: 0, textTransform: `uppercase` }}
+      >
+        Proposal
+      </Styled.h1>
+      <Styled.h2 sx={{ fontFamily: `body`, fontSize: 40 }}>
+        Granada, Spain
+      </Styled.h2>
+      <Styled.p>
+        Dustin proposed (and Maggie <em>shockingly</em> said yes!) with both of
+        their families on January 19th, 2020.
+      </Styled.p>
+    </Styled.div>
+  )
+}
