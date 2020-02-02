8d81119024810dbbf9159e164876109a3497ad9b
Dustin Schau <dustinschau@gmail.com>
2020-02-01 23:56:24 -0800

    chore: update

--- site/src/components/wedding-party.js
+++ site/src/components/wedding-party.js
@@ -1,29 +1,72 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import { useState } from 'react'
 
-import Person from './person'
+import Person, { personFragment } from './person'
+
+const filter = (people, customFilterFn) => people.filter(customFilterFn)
 
 function WeddingParty({ bridesParty, groomsParty }) {
-  const party = bridesParty.reduce((merged, member, index) => {
-    return merged.concat(member).concat(groomsParty[index])
+  const peeps = bridesParty.reduce((merged, member, index) => {
+    return merged
+      .concat({
+        ...member,
+        bride: true,
+      })
+      .concat({
+        ...groomsParty[index],
+        groom: true,
+      })
   }, [])
-  const names = party.map((member, index) =>
-    index + 1 === party.length ? `and ${member.name}` : member.name
-  )
+  const [party, setParty] = useState(peeps)
   return (
-    <div
-      sx={{
-        display: 'grid',
-        gridTemplateColumns: ['1fr', null, 'repeat(2, 1fr)'],
-      }}
-    >
-      {party.map(person => (
-        <Person
-          sx={{ mt: [2, null, 4], mb: [2, null, 4], ml: 4, mr: 4 }}
-          key={person.name}
-          {...person}
-        />
-      ))}
+    <div>
+      {process.env.GATSBY_USE_WEDDING_PARTY_FORM === `true` && (
+        <form onSubmit={ev => ev.preventDefault()}>
+          <input
+            type="text"
+            onChange={ev => {
+              const value = ev.target.value
+              setParty(
+                filter(peeps, person =>
+                  value ? new RegExp(value, `i`).test(person.name) : true
+                )
+              )
+            }}
+          />
+          <button
+            onClick={() =>
+              setParty(filter(peeps, person => person.bride === true))
+            }
+          >
+            Bridesmaids
+          </button>
+          <button
+            onClick={() =>
+              setParty(filter(peeps, person => person.groom === true))
+            }
+          >
+            Groomsman
+          </button>
+          <button type="reset" onClick={() => setParty(peeps)}>
+            Reset
+          </button>
+        </form>
+      )}
+      <div
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: ['1fr', null, 'repeat(2, 1fr)'],
+        }}
+      >
+        {party.map(person => (
+          <Person
+            sx={{ mt: [2, null, 4], mb: [2, null, 4], ml: 4, mr: 4 }}
+            key={person.name}
+            {...person}
+          />
+        ))}
+      </div>
     </div>
   )
 }
