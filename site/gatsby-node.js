9b19621031ea9d3a843a6da351362e224d825245
Dustin Schau <dustinschau@gmail.com>
2020-02-24 17:30:23 -0800

    chore: try functions

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,4 +1,13 @@
 const slugify = require('limax')
+const path = require('path')
+const fs = require('fs-extra')
+
+exports.onPostBuild = async () => {
+  await fs.copy(
+    path.join(__dirname, `functions`),
+    path.join(__dirname, `public`, `functions`)
+  )
+}
 
 exports.onCreateNode = function onCreateNode({ actions, node }) {
   if (node.internal.type === 'ContentfulBlogPost') {
