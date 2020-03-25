474c9fc9016c2c1598edc927f01b8f7bb3b04406
Dustin Schau <dustinschau@gmail.com>
2020-03-25 07:57:17 -0700

    chore: update

--- site/src/components/page.js
+++ site/src/components/page.js
@@ -56,7 +56,12 @@ function Page({
       />
       {page.hero &&
         page.hero
-          .filter(img => !!img.hero)
+          .filter(
+            img =>
+              img.hero &&
+              img.hero.localFile &&
+              img.hero.localFile.childImageSharp
+          )
           .map(img => (
             <Image
               key={img.hero.id}
