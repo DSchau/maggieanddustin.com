5c55cdf96da8d5104a443ca96c058b40a0475208
Dustin Schau <dustinschau@gmail.com>
2021-02-22 15:26:15 -0800

    chore: debug in production

--- site/src/api/hello-world.js
+++ site/src/api/hello-world.js
@@ -3,11 +3,11 @@ const twilio = require('twilio')
 const client = twilio(process.env.GATSBY_TWILIO_ACCOUNT_SID, process.env.GATSBY_TWILIO_AUTH_TOKEN)
 
 const message = async (req, res) => {
-  await client.messages.create({
-    body: `We've received your RSVP to the wedding! Thanks so much!`,
-    messagingServiceSid: process.env.GATSBY_TWILIO_MESSAGING_SID,
-    to: `+17122123001`
-  })
+  // await client.messages.create({
+  //   body: `We've received your RSVP to the wedding! Thanks so much!`,
+  //   messagingServiceSid: process.env.GATSBY_TWILIO_MESSAGING_SID,
+  //   to: `+17122123001`
+  // })
 
   return res.json({
     success: true,
