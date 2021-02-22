7bd8fb6aee90d0067664e60db90cf1e906f359ce
Dustin Schau <dustinschau@gmail.com>
2021-02-22 15:55:55 -0800

    feat: add schema

--- site/src/api/send-sms.js
+++ site/src/api/send-sms.js
@@ -1,13 +1,29 @@
 const twilio = require('twilio')
+const yup = require('yup')
 
 const client = twilio(process.env.GATSBY_TWILIO_ACCOUNT_SID, process.env.GATSBY_TWILIO_AUTH_TOKEN)
 
+const bodySchema = yup.object().shape({
+  message: yup.string().required(),
+  to: yup.string().required()
+})
+
 const message = async (req, res) => {
+  if (req.method !== 'POST') {
+    return req.json({
+      statusCode: 405,
+      message: 'Please send a POST request'
+    })
+  }
   try {
+    /*
+     * TODO: validate auth/bearer tokens
+     */
+    const { message, to } = await bodySchema.validate(req.body)
     await client.messages.create({
-      body: `We've received your RSVP to the wedding! Thanks so much!`,
+      body: message,
       messagingServiceSid: process.env.GATSBY_TWILIO_MESSAGING_SID,
-      to: `+17122123001`
+      to
     })
 
     return res.json({
@@ -20,7 +36,7 @@ const message = async (req, res) => {
     })
   } catch (e) {
     return res.json({
-      success: false,
+      statusCode: 500,
       message: e.stack
     })
   }
