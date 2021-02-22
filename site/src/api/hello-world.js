ed29d2606a268d84f0dc2a3017a9982c7120eed9
Dustin Schau <dustinschau@gmail.com>
2021-02-22 15:04:51 -0800

    chore: deploy again

--- site/src/api/hello-world.js
+++ site/src/api/hello-world.js
@@ -1,5 +1,17 @@
-module.exports = (req, res) => {
+const twilio = require('twilio')
+
+const client = twilio(process.env.GATSBY_TWILIO_ACCOUNT_SID, process.env.GATSBY_TWILIO_AUTH_TOKEN)
+
+const message = async (req, res) => {
+  await client.messages.create({
+    body: `Hello World`,
+    messagingServiceSid: process.env.GATSBY_TWILIO_MESSAGING_SID,
+    to: `7122123001`
+  })
+
   return res.json({
-    message: 'Hello'
+    success: true,
   })
 }
+
+module.exports = message
