271e102cdbd6f0993fd7c6702f9700340259fa38
Dustin Schau <dustinschau@gmail.com>
2021-10-24 11:28:40 -0700

    chore: upgrade the planet

--- src/components/color-mode.js
+++ src/components/color-mode.js
@@ -37,48 +37,50 @@ const ColorModeToggle = ({ nextMode, mode, onClick, ...rest }) => {
       {...rest}
     >
       <div
-        sx={(theme) => ({
-          position: `relative`,
-          width: `24px`,
-          height: `24px`,
-          borderRadius: `50%`,
-          border: (t) => (isDark ? `4px solid ${t.colors.text}` : `none`),
-          backgroundColor: isDark
-            ? theme.colors.modes[nextMode].background
-            : `transparent`,
-          transform: isDark ? `scale(0.55)` : `scale(1)`,
-          transition: `all 0.45s ease`,
-          overflow: isDark ? `visible` : `hidden`,
-          boxShadow: (t) =>
-            isDark ? `none` : `inset 8px -8px 0px 0px ${t.colors.text}`,
-          '&:before': {
-            content: `""`,
-            position: `absolute`,
-            right: `-9px`,
-            top: `-9px`,
-            height: `24px`,
+        sx={(theme) =>
+          console.log(theme) || {
+            position: `relative`,
             width: `24px`,
-            border: (t) => (isDark ? `2px solid ${t.colors.text}` : `none`),
+            height: `24px`,
             borderRadius: `50%`,
-            transform: isDark ? `translate(14px, -14px)` : `translate(0, 0)`,
-            opacity: isDark ? 0 : 1,
-            transition: `transform 0.45s ease`,
-          },
-          '&:after': {
-            content: `""`,
-            width: `8px`,
-            height: `8px`,
-            borderRadius: `50%`,
-            margin: `-4px 0 0 -4px`,
-            position: `absolute`,
-            top: `50%`,
-            left: `50%`,
+            border: (t) => (isDark ? `4px solid ${t.colors.text}` : `none`),
+            backgroundColor: isDark
+              ? theme.rawColors.modes[nextMode].background
+              : `transparent`,
+            transform: isDark ? `scale(0.55)` : `scale(1)`,
+            transition: `all 0.45s ease`,
+            overflow: isDark ? `visible` : `hidden`,
             boxShadow: (t) =>
-              `0 -23px 0 ${t.colors.text}, 0 23px 0 ${t.colors.text}, 23px 0 0 ${t.colors.text}, -23px 0 0 ${t.colors.text}, 15px 15px 0 ${t.colors.text}, -15px 15px 0 ${t.colors.text}, 15px -15px 0 ${t.colors.text}, -15px -15px 0 ${t.colors.text}`,
-            transform: isDark ? `scale(1)` : `scale(0)`,
-            transition: `all 0.35s ease`,
-          },
-        })}
+              isDark ? `none` : `inset 8px -8px 0px 0px ${t.colors.text}`,
+            '&:before': {
+              content: `""`,
+              position: `absolute`,
+              right: `-9px`,
+              top: `-9px`,
+              height: `24px`,
+              width: `24px`,
+              border: (t) => (isDark ? `2px solid ${t.colors.text}` : `none`),
+              borderRadius: `50%`,
+              transform: isDark ? `translate(14px, -14px)` : `translate(0, 0)`,
+              opacity: isDark ? 0 : 1,
+              transition: `transform 0.45s ease`,
+            },
+            '&:after': {
+              content: `""`,
+              width: `8px`,
+              height: `8px`,
+              borderRadius: `50%`,
+              margin: `-4px 0 0 -4px`,
+              position: `absolute`,
+              top: `50%`,
+              left: `50%`,
+              boxShadow: (t) =>
+                `0 -23px 0 ${t.colors.text}, 0 23px 0 ${t.colors.text}, 23px 0 0 ${t.colors.text}, -23px 0 0 ${t.colors.text}, 15px 15px 0 ${t.colors.text}, -15px 15px 0 ${t.colors.text}, 15px -15px 0 ${t.colors.text}, -15px -15px 0 ${t.colors.text}`,
+              transform: isDark ? `scale(1)` : `scale(0)`,
+              transition: `all 0.35s ease`,
+            },
+          }
+        }
       />
     </button>
   )
