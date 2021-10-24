e7b742c3117d867fea7104309ec2812ae1c1480a
Dustin Schau <dustinschau@gmail.com>
2021-10-24 11:53:30 -0700

    chore: fix

--- src/components/partials/index.js
+++ src/components/partials/index.js
@@ -2,14 +2,14 @@ import BachelorParty from './bachelor-party'
 import Home from './home'
 import Proposal from './proposal'
 import Details from './details'
-import { zola, custom } from './rsvp/'
+import { zola } from './rsvp/'
 
 const lookup = {
   index: Home,
   'bachelor-party': BachelorParty,
   proposal: Proposal,
   details: Details,
-  rsvp: process.env.GATSBY_RSVP_TYPE === 'custom' ? custom : zola,
+  rsvp: zola,
 }
 
 export default (path) => {
